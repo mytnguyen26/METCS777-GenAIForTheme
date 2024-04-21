1ebe37adfb44689d90d04210f4b3a117ecbb03e1
mytnguyen26 <49925370+mytnguyen26@users.noreply.github.com>
2024-04-21 17:20:15 -0400

    feate: change back to pixel value and input ids

--- model/utils.py
+++ model/utils.py
@@ -4,7 +4,7 @@ def collate_fn(examples):
     """
     Collate Function is used to create a batch
     """
-    pixel_values = torch.stack([torch.Tensor(example["image_tensor"]) for example in examples])
+    pixel_values = torch.stack([torch.Tensor(example["pixel_values"]) for example in examples])
     pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
-    input_ids = torch.stack([torch.Tensor(example["tokens"]).to(torch.int64) for example in examples])
+    input_ids = torch.stack([torch.Tensor(example["input_ids"]).to(torch.int64) for example in examples])
     return {"pixel_values": pixel_values, "input_ids": input_ids}
