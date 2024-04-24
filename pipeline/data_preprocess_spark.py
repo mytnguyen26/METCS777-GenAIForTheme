3cb4f77b67d0964e558518c9f6ecfa676f5d6032
Stanford997 <545255309@qq.com>
2024-04-24 16:53:16 -0400

    preprocess with spark

--- pipeline/data_preprocess_spark.py
+++ pipeline/data_preprocess_spark.py
@@ -1,18 +1,20 @@
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 import os
 import boto3
 from datasets import load_dataset
 import torchvision.transforms as T
 import torchvision.transforms.functional as F
+from PIL import Image
+import io
 from transformers import CLIPTokenizerFast
 from torchvision.transforms import InterpolationMode
-import pandas as pd
-from datasets import Dataset
-import pyarrow.parquet as pq
+import s3fs
+from datasets import disable_caching
+disable_caching()
 
-# Initialize Spark context
-sc = SparkContext(appName="PySpark Image Processing with RDDs")
+conf = SparkConf().setAppName("PySpark Image Processing with RDDs")
+sc = SparkContext(conf=conf)
 # Initialize Spark session
 spark = SparkSession.builder \
     .appName("PySpark Image Processing with RDDs and DataFrames") \
@@ -95,14 +97,15 @@ def resize_and_pad_then_resize(img, final_size=(512, 512), padding_mode='constan
     return img
 
 
-def preprocess_image(image_path, caption):
+def preprocess_image(image, caption):
     # Setup the transformation pipeline with the updated function
     transform_pipeline = T.Compose([
         T.Lambda(lambda img: resize_and_pad_then_resize(img, final_size=(512, 512), padding_mode='constant', fill=0)),
         T.ToTensor(),
-        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        T.ToPILImage()
     ])
-    processed_image = transform_pipeline(image_path)
+    processed_image = transform_pipeline(image)
 
     # Tokenization
     tokenizer = CLIPTokenizerFast.from_pretrained("openai/clip-vit-large-patch14")
@@ -113,15 +116,52 @@ def preprocess_image(image_path, caption):
 
 # Map transformation function over RDD
 processed_rdd = rdd.map(lambda x: preprocess_image(x[0], x[1]))
-dataset = processed_rdd.collect()
 
-formatted_data = {
-    'pixel_values': [item[0].tolist() for item in dataset],
-    'input_ids': [item[1] for item in dataset]
-}
 
-df = pd.DataFrame(formatted_data)
-train_set = Dataset.from_pandas(df)
+def image_to_binary(pil_image):
+    byte_arr = io.BytesIO()
+    pil_image.save(byte_arr, format='JPEG')
+    return byte_arr.getvalue()
+
+
+processed_rdd_2 = processed_rdd.map(lambda x: (image_to_binary(x[0]), x[1]))
+
+print('RDD finished')
+
+from pyspark.sql.types import StructType, StructField, BinaryType, ArrayType, IntegerType
+
+schema = StructType([
+    StructField("image", BinaryType(), True),
+    StructField("input_ids", ArrayType(IntegerType()), True)
+])
+df = spark.createDataFrame(processed_rdd_2, schema)
+
+print('DATAFRAME finished')
+
+from datasets import Dataset, Features, Image, Value, Sequence
+
+features = Features({"image": Image(), "input_ids": Sequence(Value("int64"))})
+dataset = Dataset.from_spark(df, features=features)
+
+print('DATAFRAME to DATASET finished')
+
+from torchvision.transforms.functional import to_tensor
+import torch
+
+
+def image_to_tensor(examples):
+    # examples are a batch of 4 images
+    # we apply the transformation (reference above for what it transfomed to)
+    # then apply the tokenization
+    examples["pixel_values"] = [to_tensor(image) for image in examples["image"]]
+    return examples
+
+
+train_set = dataset.map(image_to_tensor, remove_columns=['image'], batched=True)
+
+print('DATASET finished')
+
+import pyarrow.parquet as pq
 
 # Upload to S3 parquet
 pq.write_table(train_set.data.table, 'train_set.parquet')
